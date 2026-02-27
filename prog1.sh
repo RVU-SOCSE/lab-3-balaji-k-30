4f38cb22e77293ee75cfb2891c7aacec0eb489ef
balaji k <balajikbca25@rvu.edu.in>
2026-02-27 12:47:34 +0530

    Add files via upload

---
+++ prog1.sh
@@ -0,0 +1,22 @@
+echo "jobi:display"
+date
+sleep 3
+
+echo "job2:Display current users"
+whoiam
+sleep 3
+
+echo "job3:Display calender"
+cal
+sleep 3
+
+echo "job4: list in files"
+ls
+sleep3
+
+echo "job5: Display my system date"
+date +%T
+sleep 3
+
+echo"job6"
+pwd
